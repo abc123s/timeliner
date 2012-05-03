03d0ad299bce5b10a8a2da89852f6d3766bb27e3
Peter Zhang <peter.zhang@college.harvard.edu>
2012-05-03 16:55:16 -0400

    Made private.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,14 +27,14 @@ class UsersController < ApplicationController
 
   # GET /users/new
   # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
+#  def new
+ #   @user = User.new
+#
+ #   respond_to do |format|
+ #     format.html # new.html.erb
+ #     format.json { render json: @user }
+ #   end
+ # end
 
   # GET /users/1/edit
   def edit
@@ -43,20 +43,20 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(params[:user])
+ # def create
+ #   @user = User.new(params[:user])
 
-    respond_to do |format|
-      if @user.save
-        sign_in @user
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ #   respond_to do |format|
+ #     if @user.save
+ #       sign_in @user
+ #       format.html { redirect_to root_path, notice: 'User was successfully created.' }
+ #       format.json { render json: @user, status: :created, location: @user }
+ #     else
+ #       format.html { render action: "new" }
+ #       format.json { render json: @user.errors, status: :unprocessable_entity }
+ #     end
+ #   end
+ # end
 
   # PUT /users/1
   # PUT /users/1.json
