bf379c01ccc2c891cb5348b38db99c30e5e5c740
Peter Zhang <peter.zhang@college.harvard.edu>
2012-05-02 21:15:56 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeliner::Application.config.secret_token = '97c6339cd133f5e8c16af36a7b7727c39ea5deb86bae7696b57d18b16d932277dd7656a23bb5d0503749c1ff56d5e3df76c12c496fa8d7b10beaed878c9cfea6'
